1a73c0f7f94db291c3de8020557a9aa36c3d68c0
Michael Rea <michaelrea22@gmail.com>
2016-09-09 20:10:51 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,33 @@
+#Printing a statement
+print "I will now count my chickens:"
+
+#Printing total number of Hens and Roosters
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+#Printing a statement
+print "Now I will count the eggs:"
+
+#Printing total number of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Printing a statement
+print "Is it true that  3 + 2 < 5 - 7?"
+
+#Confirming statement
+print 3 + 2 < 5 - 7
+
+#Printing answers to math questions
+print "What is 3 + 2", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#Printing a statement
+print "Oh, that's why it's False."
+
+#Printing a statement
+print "How about some more."
+
+#Printing answers to math questions
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ ex3_1.py
@@ -0,0 +1,9 @@
+print "Now I'm doing an additional program because LPTHW told me so."
+
+print "I wonder what I should find the answer to now?"
+
+print "How about some random things? Okay, here we go!"
+
+print "I wonder what the value of 3 + 2 + 1 / 4 * 5 is?", 3 + 2 + 1 / 4 * 5
+
+print "Well that was easy! I'm done for now."
